5895563a8fcbfceee6c58dfe4bd6138825f52050
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-08-13 11:30:57 +0700

    add next auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import LoginButton from "@/components/LoginButton";
 import { Button } from "@/components/ui/button";
+import { SessionProvider } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Login Bro</Button>
-    </main>
+    <SessionProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <LoginButton />
+      </main>
+    </SessionProvider>
   );
 }
