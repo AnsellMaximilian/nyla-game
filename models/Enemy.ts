451d113d568bb3157c394e244cb25188d3ffcf08
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-08-17 13:58:14 +0700

    basic enemies

---
+++ models/Enemy.ts
@@ -0,0 +1,121 @@
+import { loadImage } from "@/utils/common";
+import Game from "./Game";
+
+export class Enemy {
+  frameX: number;
+  frameY: number;
+  fps: number;
+  frameInterval: number;
+  frameTimer: number;
+  game: Game;
+  width: number = 0;
+  height: number = 0;
+  x = 0;
+  y = 0;
+  speedX = 0;
+  speedY = 0;
+  maxFrame = 5;
+  markedForDeletion = false;
+  image: CanvasImageSource | null = null;
+
+  static images: { [key: string]: CanvasImageSource } = {};
+  constructor(game: Game) {
+    this.frameX = 0;
+    this.frameY = 0;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+    this.game = game;
+  }
+
+  update(deltaTime: number) {
+    this.x -= this.speedX + this.game.speed;
+    this.y += this.speedY;
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
+
+    if (this.x + this.width < 0) this.markedForDeletion = true;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    if (this.image) {
+      ctx.drawImage(
+        this.image,
+        this.frameX * this.width,
+        0,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  static async prepareAssets() {
+    const fly = await loadImage("/images/test/enemy_fly.png");
+    const plant = await loadImage("/images/test/enemy_plant.png");
+
+    this.images["fly"] = fly;
+    this.images["plant"] = plant;
+  }
+
+  static getImages() {
+    return this.images;
+  }
+}
+
+export class FlyingEnemy extends Enemy {
+  angle = 0;
+  va = 0;
+  constructor(game: Game) {
+    super(game);
+    this.width = 60;
+    this.height = 44;
+    this.x = this.game.width + Math.random() * this.game.width * 0.5;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.speedX = Math.random() + 1;
+    this.speedY = 0;
+    this.maxFrame = 5;
+    this.image = Enemy.getImages()["fly"];
+    this.va = Math.random() * 0.1 + 0.1;
+  }
+
+  update(deltaTime: number): void {
+    super.update(deltaTime);
+    this.angle += this.va;
+    this.y += Math.sin(this.angle);
+    // console.log({ angle: this.angle, y: this.y });
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    super.draw(ctx);
+  }
+}
+
+export class GroundEnemy extends Enemy {
+  constructor(game: Game) {
+    super(game);
+    this.width = 60;
+    this.height = 67;
+    this.x = this.game.width;
+    this.y = this.game.height - this.height - this.game.groundMargin;
+    this.image = Enemy.getImages()["plant"];
+    this.speedX = 0;
+    this.speedY = 0;
+    this.maxFrame = 1;
+  }
+}
+
+export class ClimbingEnemy extends Enemy {
+  constructor(game: Game) {
+    super(game);
+  }
+  draw(): void {}
+}
--- models/Game.ts
+++ models/Game.ts
@@ -2,6 +2,7 @@ import { loadImage } from "@/utils/common";
 import Player from "./Player";
 import { InputHandler } from "./InputHandler";
 import { Background } from "./Background";
+import { Enemy, FlyingEnemy, GroundEnemy } from "./Enemy";
 
 class Game {
   width: number;
@@ -18,6 +19,9 @@ class Game {
 
   speed: number;
   maxSpeed: number;
+  enemies: Enemy[];
+  enemyTimer = 0;
+  enemyInterval = 1000;
 
   constructor(
     width: number,
@@ -39,25 +43,53 @@ class Game {
     this.inputHandler = new InputHandler(this);
 
     this.player = new Player(this);
+
+    this.enemies = [];
   }
 
   update(deltaTime: number) {
     this.background.update();
     this.player.update(this.inputHandler.keys, deltaTime);
+
+    // enemies
+    if (this.enemyTimer > this.enemyInterval) {
+      this.addEnemy();
+      this.enemyTimer = 0;
+    } else {
+      this.enemyTimer += deltaTime;
+    }
+
+    this.enemies.forEach((enemy) => {
+      enemy.update(deltaTime);
+      if (enemy.markedForDeletion)
+        this.enemies.splice(this.enemies.indexOf(enemy), 1);
+    });
   }
 
   draw() {
     this.background.draw(this.context);
     this.player.draw(this.context);
+
+    this.enemies.forEach((enemy) => {
+      // console.log(enemy.y);
+      enemy.draw(this.context);
+    });
   }
 
   async prepareAssets() {
     await this.player.prepareAssets();
     await this.background.prepareAssets();
+    await Enemy.prepareAssets();
   }
 
   cleanUp() {
     this.inputHandler.cleanUp();
+  }
+
+  addEnemy() {
+    if (this.speed > 0 && Math.random() < 0.5)
+      this.enemies.push(new GroundEnemy(this));
+    this.enemies.push(new FlyingEnemy(this));
   }
 }
 
